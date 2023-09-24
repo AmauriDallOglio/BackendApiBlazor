e670a1f9937418d13efa1d04014423df75b48c88
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2023-09-24 15:04:05 -0300

    Adicionar arquivos de projeto.

---
+++ BackendApiBlazor/Data/Tenant.cs
@@ -0,0 +1,13 @@
+﻿namespace BackendApiBlazor.Data
+{
+    public class Tenant
+    {
+        public Guid Id { get; set; }
+        public string Referencia { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public bool Inativo { get; set; }
+        public Guid? Id_Imagem { get; set; }
+    }
+
+
+}
